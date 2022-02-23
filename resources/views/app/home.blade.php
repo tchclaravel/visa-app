13d122bd37591a338e62a8e8e5b49fb9dda0f4b5
Smart Link <smartlink.com@gmail.com>
2022-02-23 23:06:25 +0200

    make some changes in home page design

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -62,11 +62,14 @@
         </div>
         <ul class="timeline">
             <li>
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('app/template/design/step-1.png')}}" alt="..." /></div>
+                <div class="timeline-image">
+                    <div class="step-number">
+                        1
+                    </div>
+                </div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
                         <h4 class="subheading">إختيار الوجهة</h4>
-                        {{-- <h4 class="subheading">Our Humble Beginnings</h4> --}}
                     </div>
                     <div class="timeline-body">
                         <p class="text-muted">
@@ -76,10 +79,13 @@
                 </div>
             </li>
             <li class="timeline-inverted">
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('app/template/design/step-2.png')}}" alt="..." /></div>
+                <div class="timeline-image">
+                    <div class="step-number">
+                        2
+                    </div>
+                </div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
-                        {{-- <h4>March 2011</h4> --}}
                         <h4 class="subheading">تعبئة بيانات المسافرين</h4>
                     </div>
                     <div class="timeline-body">
@@ -90,10 +96,13 @@
                 </div>
             </li>
             <li>
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('app/template/design/step-3.png')}}" alt="..." /></div>
+                <div class="timeline-image">
+                    <div class="step-number">
+                        3
+                    </div>
+                </div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
-                        {{-- <h4>December 2015</h4> --}}
                         <h4 class="subheading">تحديد الموعد</h4>
                     </div>
                     <div class="timeline-body">
@@ -104,10 +113,13 @@
                 </div>
             </li>
             <li class="timeline-inverted">
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="{{asset('app/template/design/step-4.png')}}" alt="..." /></div>
+                <div class="timeline-image">
+                    <div class="step-number">
+                        4
+                    </div>
+                </div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
-                        {{-- <h4>July 2020</h4> --}}
                         <h4 class="subheading">إختيار طريقة الدفع</h4>
                     </div>
                     <div class="timeline-body">
@@ -119,11 +131,13 @@
             </li>
             <li>
                 <div class="timeline-image">
-                    <i class="bi bi-check-circle-fill"></i>
+                    <div class="step-number bg-success">
+                        <i class="fa fa-check mt-3"></i>
+                    </div>
                 </div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
-                        <h4 style="margin-top: 23px" class="subheading">مبروك! أكتمل طلبك</h4>
+                        <h4 class="subheading text-success"> مبروك!</h4>
                     </div>
                     <div class="timeline-body">
                         <p class="text-muted">
@@ -135,69 +149,8 @@
         </ul>
     </div>
 </section>
-<!-- Team-->
-{{-- <section class="page-section bg-light" id="team">
-    <div class="container">
-        <div class="text-center">
-            <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-        </div>
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="{{asset('app/template/assets/img/team/1.jpg')}}" alt="..." />
-                    <h4>Parveen Anand</h4>
-                    <p class="text-muted">Lead Designer</p>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="{{asset('app/template/assets/img/team/2.jpg')}}" alt="..." />
-                    <h4>Diana Petersen</h4>
-                    <p class="text-muted">Lead Marketer</p>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="{{asset('app/template/assets/img/team/3.jpg')}}" alt="..." />
-                    <h4>Larry Parker</h4>
-                    <p class="text-muted">Lead Developer</p>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
-        </div>
-    </div>
-</section> --}}
-<!-- Clients-->
-{{-- <div class="py-5">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md-3 col-sm-6 my-3">
-                <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="{{asset('app/template/assets/img/logos/microsoft.svg')}}" alt="..." /></a>
-            </div>
-            <div class="col-md-3 col-sm-6 my-3">
-                <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="{{asset('app/template/assets/img/logos/google.svg')}}" alt="..." /></a>
-            </div>
-            <div class="col-md-3 col-sm-6 my-3">
-                <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="{{asset('app/template/assets/img/logos/facebook.svg')}}" alt="..." /></a>
-            </div>
-            <div class="col-md-3 col-sm-6 my-3">
-                <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="{{asset('app/template/assets/img/logos/ibm.svg')}}" alt="..." /></a>
-            </div>
-        </div>
-    </div>
-</div> --}}
+
+
 <!-- Contact-->
 <section class="page-section" id="contact">
     <div class="container">
--- resources/views/app/layout/include/script_links.blade.php
+++ resources/views/app/layout/include/script_links.blade.php
@@ -1,6 +1,6 @@
 {{-- <script src="https://kit.fontawesome.com/f7464c627f.js" crossorigin="anonymous"></script> --}}
 <!-- Font Awesome icons (free version)-->
-{{-- <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script> --}}
+<script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
